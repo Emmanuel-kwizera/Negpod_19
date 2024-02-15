050e3ecb6f217b6d89db8ba0604be87da5947639
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2024-02-15 15:41:19 +0200

    added command backup

--- backup-Negpod_ID.sh
+++ backup-Negpod_ID.sh
@@ -7,5 +7,7 @@ destination_dir="/path/to/destination/directory"
 remote_host="username@remote_host_ip"
 password="your_password_here"
 
+sshpass -p "$password" scp -r "$source_dir" "$remote_host:$destination_dir"
+
 
 
