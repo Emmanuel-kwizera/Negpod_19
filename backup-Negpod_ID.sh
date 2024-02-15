429b5d75be12695629aa6b48af153d9d3c564761
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2024-02-15 15:39:24 +0200

    added source directory

---
+++ backup-Negpod_ID.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Source directory on the local machine
+source_dir="/path/to/source/directory"
+
+# Destination directory on the remote server
+destination_dir="/path/to/destination/directory"
+
+
