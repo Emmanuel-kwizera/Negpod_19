347582f5bc54e5cf99a804425a876f485cc7e8f3
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2024-02-15 15:40:11 +0200

    added server  credentials

--- backup-Negpod_ID.sh
+++ backup-Negpod_ID.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Source directory on the local machine
 source_dir="/path/to/source/directory"
 
-# Destination directory on the remote server
 destination_dir="/path/to/destination/directory"
+
+remote_host="username@remote_host_ip"
+password="your_password_here"
+
 
 
