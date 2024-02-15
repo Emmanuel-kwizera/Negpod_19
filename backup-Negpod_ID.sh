41b32ca63fb2260f58c4da3c0ead5491d0675b38
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2024-02-15 15:48:39 +0200

    added directories

--- backup-Negpod_ID.sh
+++ backup-Negpod_ID.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source_dir="/path/to/source/directory"
+source_dir="./Negpod_id-q1"
 
-destination_dir="/path/to/destination/directory"
+destination_dir="/summative/1023-2024j"
 
-remote_host="username@remote_host_ip"
-password="your_password_here"
+remote_host="2f05c1f8800b@2f05c1f8800b.3be8ebfc.alu-cod.online"
+password="d4a1d225d0abda9549d8"
 
 sshpass -p "$password" scp -r "$source_dir" "$remote_host:$destination_dir"
 
