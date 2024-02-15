8b92bb162386ea87ec182e650421080964fe2dcc
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2024-02-15 16:29:54 +0200

    added working changes

--- backup-Negpod_ID.sh
+++ backup-Negpod_ID.sh
@@ -7,17 +7,14 @@ DEST_USER="2f05c1f8800b"
 DEST_DIR="/summative/1023-2024j"
 PASSWORD="d4a1d225d0abda9549d8"
 
-# Input validation (optional)
-read -p "Confirm directory copy (y/n): " confirmation
-if [[ $confirmation != "y" ]]; then
-  exit 0
+sshpass -p "$PASSWORD" scp -o StrictHostKeyChecking=no -r "$SOURCE_DIR" "$DEST_USER@$DEST_HOST:$DEST_DIR"
+
+if [ $? -ne 0 ]; then
+  echo "Failed to copy directory"
+  exit 1
+else 
+  echo "Directory copied successfully"
 fi
-
-# Error handling
-scp -r "$SOURCE_DIR" "$DEST_USER@$DEST_HOST:$DEST_DIR" || exit 1
-
-echo "Directory copied successfully."
-
 
 
 
