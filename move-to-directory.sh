b93235d6a3f720b12a5c78050a6e69ca06408861
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2024-02-15 17:23:34 +0200

    add to create folder if not exit

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,2 +1,7 @@
-#!/usr/bin/bash
-mv main.sh students-list_1023.txt select-emails.sh student-emails.txt Negpod_19-q1
+#!/bin/bash
+
+destination_dir="Negpod_19-q1"
+
+mkdir -p "$destination_dir"
+
+mv main.sh students-list_1023.txt select-emails.sh student-emails.txt "$destination_dir/"
